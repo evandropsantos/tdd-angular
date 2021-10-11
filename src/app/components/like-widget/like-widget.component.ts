4b7b447f4a0032cdb5c1976f532c0716b747451c
Evandro P. Santos <evandropsantos@gmail.com>
2021-10-11 09:18:22 -0300

    chore(struct): Teste de componente

--- src/app/components/like-widget/like-widget.component.ts
+++ src/app/components/like-widget/like-widget.component.ts
@@ -10,7 +10,7 @@ import { UniqueIdService } from 'src/app/shared/services/unique-id/unique-id.ser
 export class LikeWidgetComponent implements OnInit {
   @Output() public liked = new EventEmitter<void>();
   @Input() public likes = 0;
-  @Input() public id = null;
+  @Input() public id: string = null;
 
   public fonts = { faThumbsUp }
 
